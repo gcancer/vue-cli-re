75b21ff65e8cbf3a2d948a505626e780975a9b2b
gcancer <wldyd0721@naver.com>
2021-01-10 22:48:16 +0900

    vue-cli upload

---
+++ src/a.vue
@@ -0,0 +1,31 @@
+// var appHeader = {
+//     template: '<div>header</div>',
+//     methods:{
+//         addNum: function(){
+
+//         }
+//     }
+// }
+
+<template>
+<!-- HTML -->
+<div>header</div>
+  <!-- HTML -->
+</template>
+
+<script>
+export default { // es6 modules와 관련된 문법
+    // Javascript : 인스턴스 옵션
+    methods:{
+        addNum: function(){
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
+
+// 특정영역에해당하는 HTML,JAVASCRIPT,CSS를 한 페이지에 관리하겠다 -> SPA
--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 위와 아래의 역할은 같다
+// var App ={
+//   template : '<div>app</div>'
+// }
 new Vue({
-  render: h => h(App),
+  render: h => h(App), //render: Vue 내부함수 (template이라는 속성을 정의했을 때 render 함수가 실행된다)
+  // components: {
+  //   'app': App
+  // } //같은 내용
 }).$mount('#app')
+
+
+// new Vue({
+//   el : '#app',
+//   render: h => h(App), // render : App 이라는 싱글 컴포넌트를 불러와서 집어넣고 render를 했다.
+// })
+
